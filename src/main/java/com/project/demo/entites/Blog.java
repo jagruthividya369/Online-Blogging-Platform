cd8a6cf78f60925034867b9ce3890a7c953d4d5c
SJV <jagruthividya.3699@gmail.com>
2021-12-07 17:38:12 +0530

    Updated Blog Entity

--- src/main/java/com/project/demo/entites/Blog.java
+++ src/main/java/com/project/demo/entites/Blog.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Date;
 
 @Entity
 @NoArgsConstructor
@@ -20,15 +21,15 @@ public class Blog {
     private String blogName;
 
     @Length(min=10)
-    @Column(name="blogContent", nullable = false, unique = false)
+    @Column(name="blogContent", nullable = false)
     @NotEmpty(message ="Please enter the contents")
     @Lob
     private String blogContent;
 
-    @Column(name = "likes", nullable = true)
+    @Column(name = "likes", columnDefinition = "integer default 0")
     private Integer blogLikes;
 
-    @Column(name = "dislikes", nullable = true)
+    @Column(name = "dislikes", columnDefinition = "integer default 0")
     private Integer blogDisLikes;
 
     @ManyToOne
@@ -37,15 +38,20 @@ public class Blog {
     @Column(name="published_status",nullable=false,columnDefinition = "boolean default true")
     private boolean publishedStatus;   //true-published; false-draft
 
+    @Column(name = "published_on")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
 
 
-    public Blog(String blogName, String blogContent, Integer blogLikes, Integer blogDisLikes, Long id, boolean publishedStatus) {
+
+    public Blog(String blogName, String blogContent, Integer blogLikes, Integer blogDisLikes, Long id, boolean publishedStatus, Date timestamp) {
         this.blogName = blogName;
         this.blogContent = blogContent;
         this.blogLikes = blogLikes;
         this.blogDisLikes = blogDisLikes;
         this.user =new User(id,"","","","");
         this.publishedStatus=publishedStatus;
+        this.timestamp= timestamp;
 
     }
 }
